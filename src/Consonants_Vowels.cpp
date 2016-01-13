f133f29ca4241c40e9cb4fed46b22d9c55848786
chmanoj133 <chmanoj133@missionrnd.com>
2016-01-13 09:31:17 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -19,7 +19,15 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 #include <stddef.h>
 
+int len(char *str);
+void start_count(char a, int *consonants, int *vowels);
+void recur_count(char *str, int *consonants, int *vowels, int i, int j);
 
+//	TWO METHODS: 1. Without Recursion
+//				 2. With Recursion
+
+/*
+// way-1: without recursion
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels)
 {
 	int i, v = 0, c = 0;
@@ -39,4 +47,51 @@ void count_vowels_and_consonants(char *str,int *consonants, int *vowels)
 
 	*consonants = c;
 	*vowels = v;
+}
+
+*/
+
+
+// way-2: with recursion
+void count_vowels_and_consonants(char *str, int *consonants, int *vowels)
+{
+	int c = 0, v = 0;
+	if (str != NULL)
+		recur_count(str, &c, &v, 0, len(str) - 1);
+	*consonants = c;
+	*vowels = v;
+}
+
+void recur_count(char *str, int *consonants, int *vowels, int i, int j)
+{
+	int mid;
+	if (i == j)
+		start_count(str[i], consonants, vowels);
+	if (i < j)
+	{
+		mid = (i + j) / 2;
+		recur_count(str, consonants, vowels, i, mid);
+		recur_count(str, consonants, vowels, mid + 1, j);
+	}
+}
+
+void start_count(char a, int *consonants, int *vowels)
+{
+	int c = 0, v = 0;
+
+	if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+		*vowels = *vowels + 1;
+	else if (a >= 'a' && a <= 'z')
+		*consonants = *consonants + 1;
+	else if (a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U')
+		*vowels = *vowels + 1;
+	else if (a >= 'A' && a <= 'Z')
+		*consonants = *consonants + 1;
+}
+
+int len(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+	return i;
 }
